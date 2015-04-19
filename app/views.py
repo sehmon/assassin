5cdb543b2dbdb35311ce9c60eb94cc96bf6084c2
yawkity <sehmon3@gmail.com>
2015-04-19 03:49:36 -0400

    Added Twilio API Keys

--- app/views.py
+++ app/views.py
@@ -1,4 +1,4 @@
-from flask import render_template, redirect
+from flask import render_template, redirect, request
 from app import app, db, models
 from models import Player
 
@@ -8,19 +8,17 @@ from twilio.rest import TwilioRestClient
 account_sid = "AC6202a281694e94d9df7485b5d21b5dcd"
 auth_token  = "8b213cfb31f8b3e4a355fb913ebfde69"
 
-client = TwilioRestClient(account_sid, auth_token)
 
 @app.route("/", methods=['GET', 'POST'])
 def hello():
 
     players = models.Player.query.all()
 
-
-    from_number = request.values.get('From', None)
-    text = request.values.get('Body', None)
-    print(text)
-
-    # if from_number not in callers:
+    if request.method == 'POST':
+        from_number = request.values.get('From', None)
+        text = request.values.get('Body', None)
+        print(text)
+        client = TwilioRestClient(account_sid, auth_token)
 
 
     return render_template("players.html", players=players)
