c9debef6fb75baae01708ddbd4e557071cfaac8d
yawkity <sehmon3@gmail.com>
2015-04-19 03:33:51 -0400

    Added database and Twilio

--- app/models.py
+++ app/models.py
@@ -1,1 +1,14 @@
 # Boilerplate for Models
+
+from app import db
+
+class Player(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    number = db.Column(db.Integer)
+    code = db.Column(db.Integer)
+    name = db.Column(db.String(50))
+    alive = db.Column(db.Boolean)
+    score = db.Column(db.Integer)
+
+    def __repr__(self):
+        return "<Player: %r>" % (self.name)
--- app/views.py
+++ app/views.py
@@ -1,6 +1,26 @@
 from flask import render_template, redirect
-from app import app
+from app import app, db, models
+from models import Player
 
-@app.route("/")
+import twilio.twiml
+from twilio.rest import TwilioRestClient
+
+account_sid = "AC6202a281694e94d9df7485b5d21b5dcd"
+auth_token  = "8b213cfb31f8b3e4a355fb913ebfde69"
+
+client = TwilioRestClient(account_sid, auth_token)
+
+@app.route("/", methods=['GET', 'POST'])
 def hello():
-    return "Hello World!"
+
+    players = models.Player.query.all()
+
+
+    from_number = request.values.get('From', None)
+    text = request.values.get('Body', None)
+    print(text)
+
+    # if from_number not in callers:
+
+
+    return render_template("players.html", players=players)
